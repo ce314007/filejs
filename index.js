477fe21da1aa9751542153713a92ce1bc311c00b
ce314007 <simanjuntakyosep190@gmail.com>
2018-10-31 15:23:51 +0000

    add file

---
+++ index.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+var port = Number(process.env.PORT || 80);
+
+http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(port);
+
+console.log("App ready at port "+port);
